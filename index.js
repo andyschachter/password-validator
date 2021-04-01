46cc8b8c4c8b8f4fe736553f74eda2dc2822f77b
Andy Schachter <andy.schachter@gmail.com>
2021-03-31 21:42:50 -0400

    added functions for checking upper and lower cases

--- index.js
+++ index.js
@@ -4,8 +4,35 @@ const lengthCheck = (val) => {
   return false
 }
 
+const lowerCheck = (val) => {
+
+  for (let i = 0; i < val.length; i++) {
+    let character = val.charAt(i)
+
+    if (character.toUpperCase() !== character && character === character.toLowerCase()) return true
+  }
+
+  return false
+}
+
+console.log(lowerCheck('PASSWORd'))
+
+const upperCheck = (val) => {
+
+  for (let i = 0; i < val.length; i++) {
+    let character = val.charAt(i)
+
+    if (character.toLowerCase() !== character && character === character.toUpperCase()) return true
+  }
+
+  return false
+}
+
+console.log(upperCheck('passworD'))
+
+
 const validatePassword = (val) => {
-  if (lengthCheck(val)) return true
+  if (lengthCheck(val) && lowerCheck(val) && upperCheck(val)) return true
 
   return false
 }
