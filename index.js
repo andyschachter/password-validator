2363bf4f531f471f0837b665cc4e7fa87beb2cd3
Andy Schachter <andy.schachter@gmail.com>
2021-03-31 21:02:38 -0400

    added function for length of password

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const validatePassword = () => {
+const lengthCheck = (val) => {
+  if (val.length >= 8) return true
 
+  return false
+}
+
+const validatePassword = (val) => {
+  if (lengthCheck(val)) return true
+
+  return false
 }
 
 module.exports = validatePassword
